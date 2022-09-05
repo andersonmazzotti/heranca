218d081b72b7698c73ee807b7d7c21bf942ee480
Anderson Mazzotti <anderson.mazzotti@alunos.sc.senac.br>
2022-09-05 20:59:42 -0300

    Novo comentario

--- src/heranca/transporte.java
+++ src/heranca/transporte.java
@@ -13,6 +13,21 @@ public class transporte {
     
     
     private int capacidade;
+
+    public int getCapacidade() {
+        return capacidade;
+    }
+
+    public void setCapacidade(int capacidade) {
+        this.capacidade = capacidade;
+    }
+    
+    /*
+     public void setCapacidade(int capacidade) {
+        this.capacidade = capacidade;
+    }
+    */
+    
     
     
     
